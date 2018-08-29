384f759d0aa144ccf7222f670ef21017e9109df7
sheryluDuck <fer.cha.cha.cha@hotmail.com>
2018-08-29 16:23:55 -0500

    Checkout method First test, checks if status of checkout book changed to reserved.

--- src/com/twu/book/Book.java
+++ src/com/twu/book/Book.java
@@ -2,6 +2,7 @@ package com.twu.book;
 
 public class Book {
     String author;
+    String bookName;
     int publishYear;
     public enum BookAvailabilityStatus{
         RESERVED,
@@ -13,11 +14,21 @@ public class Book {
         return bookStatus;
     }
 
-    public void setBookStatus(BookAvailabilityStatus bookStatus) {
+    public Book(String author, String bookName, int publishYear, BookAvailabilityStatus bookStatus) {
+        this.author = author;
+        this.bookName = bookName;
+        this.publishYear = publishYear;
+        this.bookStatus = bookStatus;
+    }
+
+    private void setBookStatus(BookAvailabilityStatus bookStatus) {
         this.bookStatus = bookStatus;
     }
 
     public boolean isBookAvailable(){
         return this.getBookStatus()==BookAvailabilityStatus.AVAILABLE;
+    }
+    public void checkoutBook(){
+        this.setBookStatus(BookAvailabilityStatus.RESERVED);
     }
 }
--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -2,22 +2,29 @@ package com.twu.biblioteca;
 
 
 import com.twu.book.Book;
-import org.junit.Assert;
 import org.junit.Test;
 
+import static junit.framework.TestCase.*;
+
 public class BookTest {
+
+    Book libritoDisponible = new Book("Plato", "Republic", 1984, Book.BookAvailabilityStatus.AVAILABLE);
+    Book libritoPrestado = new Book("Michel Foucault", "The Order of Things", 1966, Book.BookAvailabilityStatus.RESERVED);
 
     @Test
     public void shouldReturnBookIsAvailableWhenStatusIsAvailable()
     {
-        Book librito = new Book();
-        librito.setBookStatus(Book.BookAvailabilityStatus.AVAILABLE);
-        Assert.assertTrue(librito.isBookAvailable());
+        assertTrue(libritoDisponible.isBookAvailable());
     }
     @Test
     public void shouldReturnBookIsNotAvailableWhenStatusIsDifferentFromAvailable(){
-        Book librito = new Book();
-        librito.setBookStatus(Book.BookAvailabilityStatus.RESERVED);
-        Assert.assertFalse(librito.isBookAvailable());
+
+        assertFalse(libritoPrestado.isBookAvailable());
+    }
+    @Test
+    public void shouldReturnBookStatusReservedWhenCheckingOutIfBookIsAvailable(){
+        Book libritoParaReservar = new Book("Camilo Jose Cela", "Colmena", 1950, Book.BookAvailabilityStatus.AVAILABLE);
+        libritoParaReservar.checkoutBook();
+        assertEquals(Book.BookAvailabilityStatus.RESERVED, libritoParaReservar.getBookStatus());
     }
 }
